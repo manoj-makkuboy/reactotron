2b78d4da266bed3fd2b1483840140d8ac2056b86
Richard Evans <rmevans9@gmail.com>
2019-03-14 09:16:39 -0400

    Replicate 938

--- examples/ReactotronTester/app/redux/store.ts
+++ examples/ReactotronTester/app/redux/store.ts
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux"
 import ReduxThunk from "redux-thunk"
+import createSagaMiddleware from "redux-saga"
+import { all, fork, call } from "redux-saga/effects"
 import Reactotron from "reactotron-react-native"
 
 function dummyReducer(state = { counter: 0 }, action) {
-  switch(action.type) {
+  switch (action.type) {
     case "RandomThunkAction":
       return {
         ...state,
@@ -14,9 +16,32 @@ function dummyReducer(state = { counter: 0 }, action) {
   return state
 }
 
+function test1() {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(), 2500)
+  })
+}
+
+function test2() {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(), 1500)
+  })
+}
+
+function* runCalls() {
+  yield call(test1)
+  yield all([call([test1, test2]), call(test2)])
+}
+
+function* mySaga() {
+  yield all([fork(runCalls)])
+}
+
 const rootReducer = combineReducers({ dummy: dummyReducer })
 
-const middleware = applyMiddleware(ReduxThunk)
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor: Reactotron.createSagaMonitor() })
+
+const middleware = applyMiddleware(ReduxThunk, sagaMiddleware)
 
 export default () => {
   Reactotron.createSagaMonitor()
@@ -25,10 +50,12 @@ export default () => {
     rootReducer,
     compose(
       middleware,
-      Reactotron.createEnhancer(),
-    ),
+      Reactotron.createEnhancer()
+    )
   )
   // const store = (Reactotron as any).createStore(rootReducer, compose(middleware))
+
+  sagaMiddleware.run(mySaga)
 
   return store
 }
